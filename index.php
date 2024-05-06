04063dd75e6d6a6b4566618029e34840a6df6783
lin13011 <lin13011@gmail.com>
2024-05-06 23:01:12 +0800

    畫出大概的日曆樣式

--- index.php
+++ index.php
@@ -1,24 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="#">
-  <title>萬年曆作業</title>
+  <title>Document</title>
   <style>
-   /*請在這裹撰寫你的CSS*/ 
-    
+    * {
+      box-sizing: border-box;
+    }
+
+    .container {
+      width: 80%;
+      margin: auto;
+      margin-top: 200px;
+      display: flex;
+      background-color: gray;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+
+    .title {
+      width: 100%;
+      text-align: center;
+      margin-bottom: -4px;
+      background-color: red;
+    }
+
+    .item-left {
+      text-align: center;
+      align-content: center;
+      width: 40%;
+      height: 300px;
+      font-size: 100px;
+      background: blue;
+
+    }
+
+    .item-right {
+      text-align: center;
+      width: 60%;
+      background-color: lightblue;
+    }
   </style>
 </head>
-<body>
-<h1>萬年曆</h1>  
 
-<?php
-/*請在這裹撰寫你的萬年曆程式碼*/  
-  
-  
-?>
-  
+<body>
+  <div class="container">
+    <div class="title">
+      <div class="img">
+        <img src="https://picsum.photos/1524/300/?random=10">
+      </div>
+    </div>
+    <div class="item-left">
+      <div class="left-content">
+        <?php
+        $month = 5;
+        echo $month;
+        ?>
+      </div>
+    </div>
+    <div class="item-right">
+
+    </div>
+  </div>
 </body>
+
 </html>
