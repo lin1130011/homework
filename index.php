dbfced4388d8d35eadb2c4387f3dd12a10594012
lin1130011 <lin1130011@gmail.com>
2024-05-09 16:30:26 +0800

    月曆練習

--- index.php
+++ index.php
@@ -4,65 +4,46 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>萬年曆</title>
   <style>
     * {
       box-sizing: border-box;
     }
 
-    .container {
+    .box {
       width: 80%;
       margin: auto;
-      margin-top: 200px;
+      /* height: 5000px; */
       display: flex;
-      background-color: gray;
       flex-wrap: wrap;
-      justify-content: center;
     }
 
     .title {
       width: 100%;
-      text-align: center;
-      margin-bottom: -4px;
-      background-color: red;
-    }
-
-    .item-left {
-      text-align: center;
-      align-content: center;
-      width: 40%;
       height: 300px;
-      font-size: 100px;
-      background: blue;
-
+      background: red;
+      background-image: url(./images/dargon.png);
+      background-size: cover;
     }
 
-    .item-right {
-      text-align: center;
+    .left {
+      width: 40%;
+      height: 500px;
+      background: black;
+    }
+
+    .right {
       width: 60%;
-      background-color: lightblue;
+      background: lightblue;
     }
   </style>
 </head>
 
 <body>
-  <div class="container">
-    <div class="title">
-      <div class="img">
-        <img src="https://picsum.photos/1524/300/?random=10">
-      </div>
-    </div>
-    <div class="item-left">
-      <div class="left-content">
-        <?php
-        $month = 5;
-        echo $month;
-        ?>
-      </div>
-    </div>
-    <div class="item-right">
-
-    </div>
+  <div class="box">
+    <div class="title"></div>
+    <div class="left"></div>
+    <div class="right"></div>
   </div>
 </body>
 
