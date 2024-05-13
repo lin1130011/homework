56238b44db365004c848e3207be867d4fc116c23
lin1130011 <lin1130011@gmail.com>
2024-05-13 08:22:08 +0800

    修改內容

--- check.php
+++ check.php
@@ -4,12 +4,11 @@ $month = $_GET['month'];
 
 if (empty($year) || empty($month)) {
     if (empty($year)) {
-        echo "請確認是否正確輸入年份";
+        echo "請確認是否正確輸入年份<br>";
     }
     if (empty($month)) {
-        echo "請確認是否正確輸入月份";
+        echo "請確認是否正確輸入月份<br>";
     }
-    echo "<br>";
     echo "<a href='./index.php'>回首頁</a>";
 } else {
     header("location:index.php?month=$month&year=$year");
